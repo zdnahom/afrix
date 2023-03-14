b13edbdcfa42d41b7c44c6b63dec85488c9a0a9b
Nahom <zerihunnahom@gmail.com>
2023-03-14 15:24:35 +0300

    Export getMovies function

--- src/modules/store/API.js
+++ src/modules/store/API.js
@@ -1,7 +1,6 @@
-const getMovies = async () => {
+export const getMovies = async () => {
     const res = await fetch("https://api.tvmaze.com/shows");
     const data = await res.json();
     return data.slice(0, 20);
   };
 
-  
