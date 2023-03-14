88963b8daf4d7086750e2419c5d3897f8bacd682
Nahom <zerihunnahom@gmail.com>
2023-03-14 15:22:42 +0300

    Add populateData function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import './style.css';
 
 const moviesList=document.querySelector('.movies')
 
-
+const populateData=(data)=>{
+    data.forEach((item)=>{
+        let movieCard=document.createElement('div')
+        movieCard.className="movieCard"
+        movieCard.innerHTML=`
+        <img></img>
+        <div>
+        <p>Avatar 2022</p>
+        </div>
+        <button type='button'>comments</button>
+        `
+        moviesList.appendChild(movieCard)
+    })
+}
 
