57e586326c393e16698a5528e186911282e6f1ec
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-03-15 11:28:25 +0300

    Updated pop-up window

--- src/index.js
+++ src/index.js
@@ -20,5 +20,41 @@ const populateData = async (data) => {
     moviesList.appendChild(movieCard);
   });
 };
-
+const openPopup=async (id)=>{
+  console.log(`${id} : popup opened`)
+  const movie= await getMovieDetail(id)
+  popup.innerHTML=`
+  <div class="popup-content">
+  <div class="image-container">
+    <img
+      src=${movie.image.original}
+      alt="movie pic"
+    />
+    <button type="button" class="close-detail">
+      <i class="fa-solid fa-x"></i>
+    </button>
+  </div>
+  <h2>${movie.name}</h2>
+  <ul class="movie-info">
+    <li><span>Network</span> : ${movie.network.name}</li>
+    <li><span>County</span> : ${movie.network.country['name']}</li>
+    <li><span>Genres</span> : ${movie.genres}</li>
+    <li><span>Status</span> : ${movie.status}</li>
+  </ul>
+  <h3>Comments(2)</h3>
+  <ul>
+    <li>03/11/2021 Alex : The Cooles anime that I've watched</li>
+    <li>03/11/2021 Alex : The Cooles anime that I've watched</li>
+    <li>03/11/2021 Alex : The Cooles anime that I've watched</li>
+  </ul>
+  <h3>Add Comment</h3>
+  <form action="" class="form">
+    <input type="text" placeholder="Your name" required />
+    <textarea name="" id="" cols="30" rows="10" placeholder="Your Insights" required></textarea>
+    <button type="submit">Comment</button>
+  </form>
+  </div>
+  `
+  popup.classList.remove('hide')
+}
 populateData(movies);
--- src/modules/store/API.js
+++ src/modules/store/API.js
@@ -3,4 +3,9 @@ const getMovies = async () => {
   const data = await res.json();
   return data.slice(9, 18);
 };
+const getMovieDetail= async(id)=>{
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const data = await res.json();
+  return data
+}
 export default getMovies;
