416692878ce2a56e389fdd22b7f0d7d8f5c04edb
Nahom <zerihunnahom@gmail.com>
2023-03-15 15:51:24 +0300

    Make some adjustment to getMovies

--- src/modules/store/API.js
+++ src/modules/store/API.js
@@ -1,7 +1,7 @@
 export const getMovies = async () => {
   const res = await fetch('https://api.tvmaze.com/shows');
   const data = await res.json();
-  return data.slice(9, 18);
+  return data.slice(0, 9);
 };
 export const getMovieDetail = async (id) => {
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
@@ -13,7 +13,6 @@ export const getLikes = async () => {
   const res = await fetch(
     "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/iO26MOgyDZzMsAn4LSwJ/likes"
   );
-  const data = await res.json();
-  console.log(data)
+  const data = await res.json()
   return data;
 };
